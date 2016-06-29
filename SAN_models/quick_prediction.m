aa20ff7fd2067f7c3fbc63dff1b6d3d3d56d388f
Santosh Banisetty <santoshbanisetty@nevada.unr.edu>
2016-06-29 11:24:22 -0700

    quick_prediction: loads the test files

--- SAN_models/quick_prediction.m
+++ SAN_models/quick_prediction.m
@@ -1,7 +1,17 @@
 function [ ] = quick_prediction( appobject, inappobject, selCols, threshold )
 %quick_prediction: to predict as soon as possible. 
 %   Detailed explanation goes here
+appDataset = csvread('appdata/Dataset10.txt');
+appDataset(:, 9) = appDataset(:, 2)-appDataset(:, 5);
+%normalize time
+appDataset(:, 1) = appDataset(:, 1)/max(appDataset(:, 1));
+appDataset = appDataset(:, selCols);
 
+inappDataset = csvread('inappdata/Dataset9.txt');
+inappDataset(:, 9) = inappDataset(:, 2)-inappDataset(:, 5);
+%normalize time
+inappDataset(:, 1) = inappDataset(:, 1)/max(inappDataset(:, 1));
+inappDataset = inappDataset(:, selCols);
 
 end
 
